c508081ab632893b182a872cad27b6c03785d058
minu2003 <minuriviranga@gmail.com>
2025-10-19 23:35:53 +0530

    updates

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aGArMIGN4sfHcme3DaKgQBte1BzPNfYEV8lwJGpLa8s=\"\n}"
--- .next/static/chunks/app_layout_0a548d63.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d0a6d7fe._.css"
-],
-    source: "dynamic"
-});
